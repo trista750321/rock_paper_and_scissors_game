a789defa2404143af25d042659e0c0731c0c65af
trista750321 <43119980+trista750321@users.noreply.github.com>
2021-08-13 11:19:27 -0700

    Add files via upload

---
+++ rock_paper_and_scissors.py
@@ -0,0 +1,36 @@
+import random
+choices = ["Rock", "Paper", "Scissors"]
+computer = random.choice(choices)
+player = False
+computer_score = 0
+player_score = 0
+while True:
+    player = input("Rock, Paper or Scissors? ").capitalize()
+    if player == computer:
+        print("Tie!")
+    elif player == "Rock":
+        if computer == "Paper":
+            print("You lose!", computer, "covers", player)
+            computer_score += 1
+        else:
+            print("You win!", player, "smashes", computer)
+            player_score += 1
+    elif player == "Paper":
+        if computer == "Scissors":
+            print("You lose!", computer, "cut", player)
+            computer_score += 1
+        else:
+            print("You win!", player, "covers", computer)
+            player_score += 1
+    elif player == "Scissors":
+        if computer == "Rock":
+            print("You lose!", computer, "smashes", player)
+            computer_score += 1
+        else:
+            print("You win!", player, "cut", computer)
+            player_score += 1
+    elif player == "End":
+        print("Final Scores: ")
+        print(f"CPU:{computer_score}")
+        print(f"player:{player_score}")
+        break
